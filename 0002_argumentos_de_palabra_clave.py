c2fe2ccbca14d04497f6ec2bd3ce4258de3ef189
rodrigo <42428236+fynio@users.noreply.github.com>
2020-10-07 00:49:36 -0500

    Argumentos de palabras clave

--- 0002_argumentos_de_palabra_clave.py
+++ 0002_argumentos_de_palabra_clave.py
@@ -1,2 +1,12 @@
 print("Mi nombre es", "Python.", end=" ")
 print("Monty Python.")
+
+
+print()
+print("Mi nombre es", "Python.", end="\n")
+print("Monty Python.")
+
+
+print()
+print("Mi nombre es", "Python.", end="Esto se pondrá al final de la linea")
+print("Monty Python.")
